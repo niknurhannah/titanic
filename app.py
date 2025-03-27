30e73bc99a18a1095047159539393eaf729d8fb3
niknurhannah <95344631+niknurhannah@users.noreply.github.com>
2025-03-27 23:15:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -44,12 +44,6 @@ data.head()
 X = data.drop('Survived', axis=1)
 y = data['Survived']
 
-# Split into training & test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Display shapes
-X_train.shape, X_test.shape
-
 # Train the model
 model = RandomForestClassifier(random_state=42)
 model.fit(X_train, y_train)
