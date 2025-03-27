9178efd8ecc7b696a29ac092395fbaab630a6d1a
niknurhannah <95344631+niknurhannah@users.noreply.github.com>
2025-03-27 23:01:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 import streamlit as st
-from sklearn.model_selection import train_test_split
+
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 # Load Titanic dataset
